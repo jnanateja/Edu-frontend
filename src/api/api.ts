d19fb6fff0788488e6d95f6feee7deb27bc84afa
Jnana Teja <https://jnanateja@github.com>
2026-03-16 08:36:08 -0400

    update experiment results

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,7 @@
 // src/api/api.ts
 
 export const API_BASE_URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_API_BASE_URL || ""
-    : "http://127.0.0.1:8000";
+  import.meta.env.VITE_API_BASE_URL || "";
 
 const API_BASE = API_BASE_URL;
 
